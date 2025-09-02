85d618821568cd44f72686b32a6fbb7e8c02e47d
DEIMAR YANTEC OCAMPO <7361085391@untrm.edu.pe>
2025-09-02 15:55:26 -0500

    primera clase

---
+++ prueba1.R
@@ -0,0 +1,44 @@
+# MI PRIMERA PRACTICA R
+
+ALT+-
+
+Y <- "UNTRM"
+X <- "HOLA"
+
+#F(X,Y)=X + X^2+ 2XY
+#AYUDA: FN+F1
+paste(X, Y)
+
+paste(X, Y,sep = " + ")
+paste(X, Y,sep = " + ")
+      
+paste(X, Y)
+
+# tadyverse cran}
+
+library(tidyverse)
+
+library(tidyverse)
+
+library(tidyverse)
+
+library(tidyverse)
+
+library(tidyverse)
+
+library(tidyverse)
+
+library(tidyverse)
+#pipe
+
+A1 <- "ESTUDIANTES"
+
+A-1 <- "OBJETO"
+
+notas <- c(16, 17, 19, 15)
+media <-mean(notas)
+media
+
+m <- c(16, 17, 19, 15) %>% 
+mean()
+m
